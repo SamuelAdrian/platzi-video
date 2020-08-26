726f796d132be71fa3efa43d5bc4549e8e28b076
samuel <samuel.canaviri.r@gmail.com>
2020-08-25 23:07:00 -0400

    Compomente product Creado

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,18 @@
+import { Component} from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Product } from '../product.model';
+
+@Component ({
+    selector: 'app-product',
+    templateUrl: './product.component.html'
+}
+)
+export class ProductComponent {
+    product: Product = {
+        id: '1',
+        image: 'assets/images/camiseta.png',
+        title: 'Camiseta',
+        price: 8000,
+        description: 'bla bla bla',
+    };
+}
