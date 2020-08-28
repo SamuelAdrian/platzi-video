c7f9d3cd2cfb6b86b849fbc355aa4af65fe818b7
samuel <samuel.canaviri.r@gmail.com>
2020-08-28 15:11:07 -0400

    Router Componentes y Banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,13 @@ import localeEs from '@angular/common/locales/es';
 import { registerLocaleData } from '@angular/common';
 import { ExponentialPipe } from './exponential.pipe';
 import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 registerLocaleData (localeEs);
@@ -16,7 +23,14 @@ registerLocaleData (localeEs);
   declarations: [
     AppComponent,
     ProductComponent,
-    ExponentialPipe
+    ExponentialPipe,
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
