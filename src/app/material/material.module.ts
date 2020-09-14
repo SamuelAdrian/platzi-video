08c1dc5be4ffc51192b6bb7a8206e37e57b7f454
samuel <samuel.canaviri.r@gmail.com>
2020-09-14 10:54:56 -0400

    Header Completado

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -12,10 +14,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     CommonModule,
     MatButtonModule,
     MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
   ],
   exports: [
     MatButtonModule,
     MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule { }
